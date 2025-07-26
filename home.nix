fdd8e63603d4baaa9be66a2c67d424e952527b16
Daniel Innes <daniel@brotherwolf.ca>
2025-07-26 14:30:25 -0400

    waybar

--- home.nix
+++ home.nix
@@ -3,33 +3,7 @@
 {
   home.username = "daniel";
   home.homeDirectory = "/home/daniel";
-
-  # link the configuration file in current directory to the specified location in home directory
-  # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
-
-  # link all files in `./scripts` to `~/.config/i3/scripts`
-  # home.file.".config/i3/scripts" = {
-  #   source = ./scripts;
-  #   recursive = true;   # link recursively
-  #   executable = true;  # make all files executable
-  # };
-
-  # encode the file content in nix configuration file directly
-  # home.file.".xxx".text = ''
-  #     xxx
-  # '';
-
-  # set cursor size and dpi for 4k monitor
-  # xresources.properties = {
-  #   "Xcursor.size" = 16;
-  #   "Xft.dpi" = 172;
-  # };
-
-  # Packages that should be installed to the user profile.
   home.packages = with pkgs; [
-    # here is some command line tools I use frequently
-    # feel free to add your own or remove some of them
-
     firefox
     lynx
     nnn # terminal file manager
@@ -96,45 +70,39 @@
     usbutils # lsusb
   ];
 
-  # basic configuration of git, please change to your own
-  programs.git = {
-    enable = true;
-    userName = "Daniel Innes";
-    userEmail = "daniel@brotherwolf.ca";
+  programs = {
+    waybar.enable = true
+    git = {
+      enable = true;
+      userName = "Daniel Innes";
+      userEmail = "daniel@brotherwolf.ca";
+    };
+
+    starship = {
+      enable = true;
+      settings = {
+        add_newline = false;
+        aws.disabled = true;
+        gcloud.disabled = true;
+        line_break.disabled = true;
+      };
+
+    bash = {
+      enable = true;
+      enableCompletion = true;
+      bashrcExtra = ''
+        export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
+      '';
+      shellAliases = {
+        nixr = "nixos-rebuild switch --flake github:Daniel-W-Innes/nixos";
+      };
   };
 
-  # starship - an customizable prompt for any shell
-  programs.starship = {
-    enable = true;
-    # custom settings
-    settings = {
-      add_newline = false;
-      aws.disabled = true;
-      gcloud.disabled = true;
-      line_break.disabled = true;
-    };
-  };
-
-  programs.bash = {
-    enable = true;
-    enableCompletion = true;
-    bashrcExtra = ''
-      export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
-    '';
-    shellAliases = {
-      nixr = "nixos-rebuild switch --flake github:Daniel-W-Innes/nixos";
-    };
+  xdg.configFile = {
+    "waybar/config.jsonc".source  = ./waybar/config.jsonc;
+    "waybar/style.css".source = ./waybar/style.css;
   };
 
   programs.home-manager.enable = true;
-
-  # This value determines the home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update home Manager without changing this value. See
-  # the home Manager release notes for a list of state version
-  # changes in each release.
   home.stateVersion = "25.05";
 }
