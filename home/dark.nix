0ccb015fe379a54c9f28268ae4d84ab91ce8e758
Daniel Innes <daniel@brotherwolf.ca>
2025-08-08 20:47:13 -0400

    remove background

--- home/dark.nix
+++ home/dark.nix
@@ -2,9 +2,6 @@
 
 {
   dconf.settings = {
-    "org/gnome/desktop/background" = {
-      picture-uri-dark = "file://${pkgs.nixos-artwork.wallpapers.nineish-dark-gray.src}";
-    };
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
     };
@@ -14,7 +11,7 @@
     enable = true;
     theme = {
       name = "Adwaita-dark";
-      package = pkgs.gnome.gnome-themes-extra;
+      package = pkgs.gnome-themes-extra;
     };
   };
 }
