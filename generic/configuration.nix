5c12ac70434430869bd5eb2081f6a011385886b0
Daniel Innes <daniel@brotherwolf.ca>
2025-08-20 20:29:08 -0400

    prometheus node exporter

--- generic/configuration.nix
+++ generic/configuration.nix
@@ -46,4 +46,10 @@
     platformTheme = "gnome";
     style = "adwaita-dark";
   };
+  services.prometheus.exporters.node = {
+    enable = true;
+    port = 9100;
+    openFirewall = true;
+    firewallFilter = "-i br0 -p tcp -m tcp --dport 9100";
+  };
 }
