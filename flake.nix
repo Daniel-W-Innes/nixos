6e660aadf9144999ec15402d18c5399eae1b5f2c
Daniel Innes <daniel@brotherwolf.ca>
2025-08-29 17:50:41 -0400

    remove server

--- flake.nix
+++ flake.nix
@@ -51,7 +51,7 @@
             ./onion/configuration.nix
             ./generic/configuration.nix
             ./virt/podman.nix
-            ./virt/server.nix
+            #./virt/server.nix
             ./generic/autoUpgrade.nix
             home-manager.nixosModules.home-manager
             {
