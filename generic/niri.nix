3dd42287aeaf412f3690d25f37d74b813166a9ba
Daniel Innes <daniel@brotherwolf.ca>
2025-11-22 14:39:22 -0500

    init

---
+++ generic/niri.nix
@@ -0,0 +1,13 @@
+{ pkgs, ... }:
+
+{
+  programs.niri.enable = true;
+  environment.systemPackages = with pkgs; [ 
+    xwayland-satellite
+  ];
+  qt = {
+    enable = true;
+    platformTheme = "gnome";
+    style = "adwaita-dark";
+  };
+}
