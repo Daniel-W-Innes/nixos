f0e8e83a098fd35ac467f079c0101a153ffb9338
Daniel Innes <daniel@brotherwolf.ca>
2025-12-14 09:25:03 -0500

    torrents wip

---
+++ generic/server/arr.nix
@@ -0,0 +1,29 @@
+_:
+
+{
+  services = {
+    jellyfin = {
+      enable = true;
+    };
+
+    radarr = {
+      enable = true;
+    };
+
+    sonarr = {
+      enable = true;
+    };
+
+    lidarr = {
+      enable = true;
+    };
+
+    prowlarr = {
+      enable = true;
+    };
+
+    flaresolverr = {
+      enable = true;
+    };
+  };
+}
