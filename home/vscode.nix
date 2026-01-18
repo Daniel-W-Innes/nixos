71dd366413d05e8e8f5f7419d3374cac9d7cb713
Daniel Innes <daniel@brotherwolf.ca>
2026-01-18 10:37:15 -0500

    agent and git

--- home/vscode.nix
+++ home/vscode.nix
@@ -4,9 +4,12 @@ _:
   programs.vscode = {
     enable = true;
     profiles.default.userSettings = {
+      "workbench.colorTheme" = "IntelliJ IDEA Islands Dark";
+      "chat.agent.enabled" = false;
       "editor.formatOnSave" = true;
       "editor.inlineSuggest.enabled" = true;
       "merge-conflict.autoNavigateNextConflict.enabled" = true;
+      "git.autofetch" = true;
     };
   };
 }
