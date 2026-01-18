d04fa699be984802ece837c54e0073b4854eff3a
Daniel Innes <daniel@brotherwolf.ca>
2026-01-18 10:51:33 -0500

    confirmSync

--- home/vscode.nix
+++ home/vscode.nix
@@ -6,10 +6,10 @@ _:
     profiles.default.userSettings = {
       "workbench.colorTheme" = "IntelliJ IDEA Islands Dark";
       "chat.agent.enabled" = false;
-      "editor.formatOnSave" = true;
       "editor.inlineSuggest.enabled" = true;
       "merge-conflict.autoNavigateNextConflict.enabled" = true;
       "git.autofetch" = true;
+      "git.confirmSync" = true;
     };
   };
 }
