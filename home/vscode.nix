7e602e7e7f8c4c4013c93724a90b66b17d3b6039
Daniel Innes <daniel@brotherwolf.ca>
2026-01-18 10:14:23 -0500

    profiles init

--- home/vscode.nix
+++ home/vscode.nix
@@ -3,5 +3,11 @@ _:
 {
   programs.vscode = {
     enable = true;
+    profiles.default.userSettings = {
+      "editor.fontFamily" = "'Jetbrains Mono Nerd Font'; 'monospace'; monospace";
+      "editor.formatOnSave" = true;
+      "editor.inlineSuggest.enabled" = true;
+      "merge-conflict.autoNavigateNextConflict.enabled" = true;
+    };
   };
 }
