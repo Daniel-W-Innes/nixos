bf633d4d25324be25bbfebbe9205a9e6db5b6a74
Daniel Innes <daniel@brotherwolf.ca>
2026-01-18 10:29:45 -0500

    remove font

--- home/vscode.nix
+++ home/vscode.nix
@@ -4,7 +4,6 @@ _:
   programs.vscode = {
     enable = true;
     profiles.default.userSettings = {
-      "editor.fontFamily" = "'Jetbrains Mono Nerd Font'; 'monospace'; monospace";
       "editor.formatOnSave" = true;
       "editor.inlineSuggest.enabled" = true;
       "merge-conflict.autoNavigateNextConflict.enabled" = true;
