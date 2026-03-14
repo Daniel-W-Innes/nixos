d3f8fa10c6167cf4a753c20ea180191284ff7e32
Daniel Innes <daniel@brotherwolf.ca>
2026-03-14 15:08:19 -0400

    visibility grafana setup

--- secrets/age.nix
+++ secrets/age.nix
@@ -26,24 +26,31 @@ _:
     #   group = "copyparty";
     # };
 
-    # prom-copyparty-metrics = {
-    #   file = ./copyparty-metrics.age;
-    #   owner = "prometheus";
-    #   group = "prometheus";
+    prom-copyparty-metrics = {
+      file = ./copyparty-metrics.age;
+      owner = "prometheus";
+      group = "prometheus";
+    };
+
+    grafana-admin-password = {
+      file = ./grafana-admin-password.age;
+      owner = "grafana";
+      group = "grafana";
+      mode = "0400";
+    };
+
+    # gluetun-wireguard = {
+    #   file = ./gluetun-wireguard.env.age;
+    #   owner = "root";
+    #   group = "root";
+    #   mode = "0400";
     # };
 
-    gluetun-wireguard = {
-      file = ./gluetun-wireguard.env.age;
-      owner = "root";
-      group = "root";
-      mode = "0400";
-    };
-
-    qbittorrent-webui-password-pbkdf2 = {
-      file = ./qbittorrent-webui-password-pbkdf2.age;
-      owner = "root";
-      group = "root";
-      mode = "0400";
-    };
+    # qbittorrent-webui-password-pbkdf2 = {
+    #   file = ./qbittorrent-webui-password-pbkdf2.age;
+    #   owner = "root";
+    #   group = "root";
+    #   mode = "0400";
+    # };
   };
 }
