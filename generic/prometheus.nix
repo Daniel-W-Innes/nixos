ce5a4fff32661a04b988decbd800db02264b71d1
Daniel Innes <daniel@brotherwolf.ca>
2026-03-29 13:21:00 -0400

    exporters

--- generic/prometheus.nix
+++ generic/prometheus.nix
@@ -26,77 +26,57 @@ _:
       </service>
     </service-group>
     '';
-    "avahi/services/cadvisor.service".text = ''<?xml version="1.0" standalone='no'?>
+    "avahi/services/process-exporter.service".text = ''<?xml version="1.0" standalone='no'?>
     <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
     <service-group>
-      <name replace-wildcards="yes">cAdvisor on %h</name>
+      <name replace-wildcards="yes">Process Exporter on %h</name>
       <service>
         <type>_http._tcp</type>
-        <subtype>_cadvisor._sub._http._tcp</subtype>
-        <port>9580</port>
+        <subtype>_process-exporter._sub._http._tcp</subtype>
+        <port>9256</port>
         <txt-record>path=/metrics</txt-record>
       </service>
     </service-group>
     '';
-    "avahi/services/systemd-exporter.service".text = ''<?xml version="1.0" standalone='no'?>
+    "avahi/services/nvidia-gpu-exporter.service".text = ''<?xml version="1.0" standalone='no'?>
     <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
     <service-group>
-      <name replace-wildcards="yes">Systemd Exporter on %h</name>
+      <name replace-wildcards="yes">Nvidia-gpu Exporter on %h</name>
       <service>
         <type>_http._tcp</type>
-        <subtype>_systemd-exporter._sub._http._tcp</subtype>
-        <port>9558</port>
+        <subtype>_nvidia-gpu._sub._http._tcp</subtype>
+        <port>9835</port>
         <txt-record>path=/metrics</txt-record>
       </service>
     </service-group>
     '';
   };
-  networking.firewall.interfaces."br0".allowedTCPPorts = [
-    9580
-  ];
   services = {
     prometheus.exporters = {
       node = {
         enable = true;
         port = 9100;
         openFirewall = true;
-        firewallFilter = "-i br0 -p tcp -m tcp --dport 9100";
+        firewallFilter = "-i enp8s0 -p tcp -m tcp --dport 9100";
       };
       process = {
         enable = true;
         port = 9256;
         openFirewall = true;
-        firewallFilter = "-i br0 -p tcp -m tcp --dport 9256";
+        firewallFilter = "-i enp8s0 -p tcp -m tcp --dport 9256";
       };
       smartctl = {
         enable = true;
         port = 9633;
         openFirewall = true;
-        firewallFilter = "-i br0 -p tcp -m tcp --dport 9633";
+        firewallFilter = "-i enp8s0 -p tcp -m tcp --dport 9633";
       };
       nvidia-gpu = {
         enable = true;
         port = 9835;
         openFirewall = true;
-        firewallFilter = "-i br0 -p tcp -m tcp --dport 9835";
+        firewallFilter = "-i enp8s0 -p tcp -m tcp --dport 9835";
       };
-      # systemd = {
-      #   enable = true;
-      #   port = 9558;
-      #   openFirewall = true;
-      #   firewallFilter = "-i br0 -p tcp -m tcp --dport 9558";
-      # };
-      # borgmatic = {
-      #   enable = true;
-      #   port = 9996;
-      #   openFirewall = true;
-      #   user = "root";
-      #   firewallFilter = "-i br0 -p tcp -m tcp --dport 9996";
-      # };
-    };
-    cadvisor = {
-      enable = true;
-      port = 9580;
     };
   };
 }
