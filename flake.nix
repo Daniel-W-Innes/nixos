e925d21ffcefe4af13e4f24f557eae9bfe4d90e9
Daniel Innes <daniel@brotherwolf.ca>
2026-04-11 12:16:42 -0400

    avahi

--- flake.nix
+++ flake.nix
@@ -109,6 +109,7 @@
             { config.facter.reportPath = ./onion/facter.json; }
             ./onion/configuration.nix
             ./secrets/age.nix
+            ./generic/avahi.nix
             ./generic/all.nix
             ./generic/borgmatic.nix
             ./generic/zsa.nix
