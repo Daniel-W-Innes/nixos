7cb929fc660d39ef5e3367d3f093b214bb93a923
Daniel Innes <daniel@brotherwolf.ca>
2026-04-19 12:43:58 -0400

    update

--- generic/min.nix
+++ generic/min.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
 {
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_6_19;
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"
