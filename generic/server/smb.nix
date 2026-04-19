ecab676200c33d4dd4ff07b50df9c87643e24adf
Daniel Innes <daniel@brotherwolf.ca>
2026-04-19 17:16:44 -0400

    Calibre

--- generic/server/smb.nix
+++ generic/server/smb.nix
@@ -8,18 +8,34 @@
     group = "media";
   };
 
-  fileSystems."/mnt/media" = {
-    device = "//pumpkin.lc.brotherwolf.ca/Media";
-    fsType = "cifs";
-    options = [
-      "credentials=${config.age.secrets.pumpkin-smb-credentials.path}"
-      "x-systemd.automount"
-      "noauto"
-      "nofail"
-      "uid=media"
-      "gid=media"
-      "file_mode=0640"
-      "dir_mode=0750"
-    ];
+  fileSystems = {
+    "/mnt/media" = {
+      device = "//pumpkin.lc.brotherwolf.ca/Media";
+      fsType = "cifs";
+      options = [
+        "credentials=${config.age.secrets.pumpkin-smb-credentials.path}"
+        "x-systemd.automount"
+        "noauto"
+        "nofail"
+        "uid=media"
+        "gid=media"
+        "file_mode=0640"
+        "dir_mode=0750"
+      ];
+    };
+    "/mnt/references" = {
+      device = "//pumpkin.lc.brotherwolf.ca/Calibre";
+      fsType = "cifs";
+      options = [
+        "credentials=${config.age.secrets.pumpkin-smb-credentials.path}"
+        "x-systemd.automount"
+        "noauto"
+        "nofail"
+        "uid=media"
+        "gid=media"
+        "file_mode=0640"
+        "dir_mode=0750"
+      ];
+    };
   };
 }
