9c368d2ee435b0e3ea1d8a1a451104ec476167df
Daniel Innes <daniel@brotherwolf.ca>
2026-04-19 16:02:03 -0400

    clean

--- generic/server/smb.nix
+++ generic/server/smb.nix
@@ -8,35 +8,18 @@
     group = "media";
   };
 
-  fileSystems = {
-    "/mnt/media" = {
-      device = "//pumpkin.lc.brotherwolf.ca/Media";
-      fsType = "cifs";
-      options = [
-        "credentials=${config.age.secrets.pumpkin-smb-credentials.path}"
-        "x-systemd.automount"
-        "noauto"
-        "nofail"
-        "uid=media"
-        "gid=media"
-        "file_mode=0640"
-        "dir_mode=0750"
-      ];
-    };
-
-    "/mnt/jellyfin" = {
-      device = "//pumpkin.lc.brotherwolf.ca/Jellyfin_configs";
-      fsType = "cifs";
-      options = [
-        "credentials=${config.age.secrets.pumpkin-smb-credentials.path}"
-        "x-systemd.automount"
-        "noauto"
-        "nofail"
-        "uid=media"
-        "gid=media"
-        "file_mode=0640"
-        "dir_mode=0750"
-      ];
-    };
+  fileSystems."/mnt/media" = {
+    device = "//pumpkin.lc.brotherwolf.ca/Media";
+    fsType = "cifs";
+    options = [
+      "credentials=${config.age.secrets.pumpkin-smb-credentials.path}"
+      "x-systemd.automount"
+      "noauto"
+      "nofail"
+      "uid=media"
+      "gid=media"
+      "file_mode=0640"
+      "dir_mode=0750"
+    ];
   };
 }
