eb0c2c623b400ac78ec085c79c5258f1c57ca70a
Daniel Innes <daniel@brotherwolf.ca>
2026-04-19 17:31:16 -0400

    listen.ip

--- generic/server/calibre.nix
+++ generic/server/calibre.nix
@@ -3,8 +3,9 @@ _:
 {
   services.calibre-web = {
     enable = true;
-    openFirewall = true;  
+    openFirewall = true;
     group = "media";
+    listen.ip = "0.0.0.0";
     options = {
       calibreLibrary = "/mnt/references";
       enableBookConversion = true;
