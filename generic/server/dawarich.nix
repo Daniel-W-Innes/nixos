981561805820f3f6f8f03a74bf5fd634dbe69999
Daniel Innes <daniel@brotherwolf.ca>
2026-04-24 13:06:38 -0400

    nominatim

--- generic/server/dawarich.nix
+++ generic/server/dawarich.nix
@@ -1,14 +1,17 @@
 _:
 
 {
-    services.dawarich = {
-      enable = true;
-      localDomain = "dawarich.lc.brotherwolf.ca";
-      configureNginx = false;
-      webPort = 3080;
-      environment = {
-        STORE_GEODATA = "true";
-        TIME_ZONE = "America/Toronto";
+    services = {
+        dawarich = {
+        enable = true;
+        localDomain = "dawarich.lc.brotherwolf.ca";
+        configureNginx = false;
+        webPort = 3080;
+        environment = {
+          STORE_GEODATA = "true";
+          TIME_ZONE = "America/Toronto";
+      };
     };
+    nominatim.enable = true;
   };
 }
