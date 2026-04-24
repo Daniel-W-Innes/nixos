79fdb609e71bb344d3f91bc1c5177c9d5f479e4e
Daniel Innes <daniel@brotherwolf.ca>
2026-04-24 15:14:40 -0400

    photon.komoot.io

--- generic/server/dawarich.nix
+++ generic/server/dawarich.nix
@@ -10,21 +10,7 @@ _:
       RAILS_ENV = "production";
       STORE_GEODATA = "true";
       TIME_ZONE = "America/Toronto";
-      PHOTON_API_HOST = "localhost:2322";
-      PHOTON_API_USE_HTTPS = "false";
-    };
-  };
-  virtualisation.oci-containers.containers = {
-    dawarich-photon = {
-      image = "rtuszik/photon-docker:latest";
-      environment = {
-        UPDATE_STRATEGY = "PARALLEL";
-        ENABLE_METRICS = "true";
-        REGION = "ca"; # TODO: This should be removed once there is more storage available for geodata.
-      };
-      ports = [
-        "127.0.0.1:2322:2322/tcp"
-      ];
+      PHOTON_API_HOST = "photon.komoot.io";
     };
   };
 }
