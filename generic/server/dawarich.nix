b4888e78927a026428d4113da9f1eb44452c6859
Daniel Innes <daniel@brotherwolf.ca>
2026-04-26 15:45:37 -0400

    add photon

--- generic/server/dawarich.nix
+++ generic/server/dawarich.nix
@@ -14,4 +14,34 @@ _:
       PHOTON_API_USE_HTTPS = "true";
     };
   };
+
+  users.groups.photon = {
+    gid = 9011;
+  };
+  users.users.photon = {
+    isSystemUser = true;
+    group = "photon";
+    uid = 9011;
+  };
+
+  systemd.tmpfiles.rules = [
+    "d /var/lib/dawarich-photon/data 0755 photon photon -"
+  ];
+
+  virtualisation.oci-containers.containers = {
+    dawarich-photon = {
+      image = "rtuszik/photon-docker:latest";
+      environment = {
+        UPDATE_STRATEGY = "PARALLEL";
+        ENABLE_METRICS = "true";
+        REGION = "ca"; # TODO: This should be removed once there is more storage available for geodata.
+      };
+      ports = [
+        "127.0.0.1:2322:2322/tcp"
+      ];
+      volumes = [
+        "/var/lib/dawarich-photon/data:/photon/data"
+      ];
+    };
+  };
 }
