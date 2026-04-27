950efc2efba38c16131ec84fbfdb3c6be0d78b8b
Daniel Innes <daniel@brotherwolf.ca>
2026-04-27 13:56:05 -0400

    transmission

--- generic/server/arr.nix
+++ generic/server/arr.nix
@@ -1,62 +1,27 @@
 { config, ... }:
 
 {
-  systemd.tmpfiles.rules = [
-    "d /var/lib/gluetun 0755 root root -"
-    "d /var/lib/nzbget 0755 root root -"
-    "d /var/lib/qbittorrent 0755 root root -"
-  ];
-  virtualisation.oci-containers.containers = {
-    "arr-gluetun" = {
-      image = "qmcgaw/gluetun:latest";
-      ports = [
-        "127.0.0.1:24682:8080/tcp"
-        "127.0.0.1:49835:6789/tcp"
-      ];
-      capabilities.NET_ADMIN = true;
-      environmentFiles = [ config.age.secrets.openvpn.path ];
-      environment = {
-        VPN_SERVICE_PROVIDER = "protonvpn";
-        SERVER_COUNTRIES = "Switzerland,Spain";
-      };
-      extraOptions = [
-        "--health-cmd=ping -c 1 www.google.com || exit 1"
-        "--health-interval=60s"
-        "--health-retries=3"
-        "--health-start-period=20s"
-        "--health-timeout=10s"
-      ];
-      volumes = [
-        "/var/lib/gluetun:/gluetun"
-      ];
-      podman.sdnotify = "healthy";
-    };
+  vpnNamespaces.proton = {
+    enable = true;
+    wireguardConfigFile = config.age.secrets.proton-vpn.path;
+    accessibleFrom = [
+      "10.8.8.0/24"
+    ];
+    portMappings = [  { from = 9091; to = 9091; } ];
+  };
 
-    "arr-nzbget" = {
-      image = "lscr.io/linuxserver/nzbget:latest";
-      dependsOn = [ "arr-gluetun" ];
-      volumes = [
-        "/mnt/media/downloads:/downloads"
-        "/var/lib/nzbget:/config"
-      ];
-      extraOptions = [
-        "--network=container:arr-gluetun"
-      ];
-    };
+  systemd.services.transmission.vpnConfinement = {
+    enable = true;
+    vpnNamespace = "proton";
+  };
 
-    "arr-qbittorrent" = {
-      image = "linuxserver/qbittorrent:latest";
-      dependsOn = [ "arr-gluetun" ];
-      environment = {
-        WEBUI_PORT = "8080";
-      };
-      volumes = [
-        "/mnt/media/downloads:/downloads"
-        "/var/lib/qbittorrent:/config"
-      ];
-      extraOptions = [
-        "--network=container:arr-gluetun"
-      ];
+  services.transmission = {
+    enable = true;
+    credentialsFile = config.age.secrets.transmission.path;
+    settings = {
+      download-dir = "/mnt/media/downloads";
+      incomplete-dir = "/mnt/media/downloads/incomplete";
+      incomplete-dir-enabled = true;
     };
   };
 }
