5c2ae19a828d9be309ae1833da8e258ca77d948e
Daniel Innes <daniel@brotherwolf.ca>
2026-04-27 15:05:09 -0400

    queue-json

--- generic/server/arr.nix
+++ generic/server/arr.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   vpnNamespaces.proton = {
@@ -15,11 +15,27 @@
     vpnNamespace = "proton";
   };
 
+  systemd.services.transmission.serviceConfig.ExecStartPre = lib.mkAfter [
+    (
+      "+"
+      + pkgs.writeShellScript "transmission-create-queue-json" ''
+        queue_json='/var/lib/transmission/.config/transmission-daemon/queue.json'
+
+        if [ ! -e "$queue_json" ]; then
+          printf '[]\n' |
+            install -D -m 600 -o transmission -g transmission /dev/stdin "$queue_json"
+        fi
+      ''
+    )
+  ];
+
   services.transmission = {
     enable = true;
     package = pkgs.transmission_4;
     credentialsFile = config.age.secrets.transmission.path;
     settings = {
+      message-level = 3;
+      encryption = 1;
       download-dir = "/mnt/media/downloads";
       incomplete-dir = "/mnt/media/downloads/incomplete";
       incomplete-dir-enabled = true;
