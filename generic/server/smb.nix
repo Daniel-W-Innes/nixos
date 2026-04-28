fb8a194ef3ae686a31d4de5131750bd8b7e08c7f
Daniel Innes <daniel@brotherwolf.ca>
2026-04-28 15:57:12 -0400

    mnt immich

--- generic/server/smb.nix
+++ generic/server/smb.nix
@@ -37,5 +37,19 @@
         "dir_mode=0770"
       ];
     };
+    "/mnt/immich" = {
+      device = "//pumpkin.lc.brotherwolf.ca/Immich";
+      fsType = "cifs";
+      options = [
+        "credentials=${config.age.secrets.pumpkin-smb-credentials.path}"
+        "x-systemd.automount"
+        "noauto"
+        "nofail"
+        "uid=media"
+        "gid=media"
+        "file_mode=0660"
+        "dir_mode=0770"
+      ];
+    };
   };
 }
