585b4ad3d643ca5761b2c2c9804e439917174eea
Daniel Innes <daniel@brotherwolf.ca>
2026-05-08 12:40:08 -0400

    settings

--- generic/server/immich.nix
+++ generic/server/immich.nix
@@ -8,5 +8,29 @@ _:
     environment = {
       IMMICH_TELEMETRY_INCLUDE = "all";
     };
+    settings = {
+      backup.database = {
+          enabled = true;
+          cronExpression = "0 02 * * *";
+          keepLastAmount = 20;
+        };
+      metadata.faces.import = true;
+      newVersionCheck.enabled = false;
+      nightlyTasks = {
+        clusterNewFaces = true;
+        databaseCleanup = true;
+        generateMemories = true;
+        missingThumbnails = true;
+        syncQuotaUsage = true;
+        startTime = "00:00";
+      };
+      passwordLogin.enabled = true;
+      reverseGeocoding.enabled = true;
+      storageTemplate = {
+        enabled = true;
+        hashVerificationEnabled = true;
+        template = "{{y}}/{{MM}}/{{dd}}/{{HH}}{{mm}}{{SSS}}_{{filename}}";
+      };
+    };
   };
 }
