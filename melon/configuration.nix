39c01a948ab67170ee8a10ba21ced6005ad18445
Daniel Innes <daniel@brotherwolf.ca>
2026-05-10 10:53:00 -0400

    zswap

--- melon/configuration.nix
+++ melon/configuration.nix
@@ -9,16 +9,26 @@
     ./hardware-configuration.nix
   ];
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot = {
+    initrd.systemd.enable = true;
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+    kernelParams = [
+      "zswap.enabled=1" # enables zswap
+      "zswap.compressor=lz4" # compression algorithm
+      "zswap.max_pool_percent=20" # maximum percentage of RAM that zswap is allowed to use
+      "zswap.shrinker_enabled=1" # whether to shrink the pool proactively on high memory pressure
+    ];
+  };
   networking.hostName = "melon";
   nixpkgs.config.allowUnfree = true;
 
-  swapDevices = [{
-    device = "/var/lib/swapfile";
-    size = 64*1024; # 64 GiB
-  }];
-  zramSwap.enable = true;
+  swapDevices = [
+    {
+      device = "/var/lib/swapfile";
+      size = 64 * 1024; # 64 GiB
+    }
+  ];
   systemd.oomd.enable = true;
 
   # This value determines the NixOS release from which the default
