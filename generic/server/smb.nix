cdd0d18ce9fa479f97d315089ae5602912f1524f
Daniel Innes <daniel@brotherwolf.ca>
2026-05-15 22:57:51 -0400

    Remove calibre

--- generic/server/smb.nix
+++ generic/server/smb.nix
@@ -1,4 +1,4 @@
-{ config, secretsDir, ... }:
+{ secretsDir, ... }:
 
 {
   age.secrets.pumpkin-smb-credentials = {
@@ -13,50 +13,5 @@
   users.users.media = {
     isSystemUser = true;
     group = "media";
-  };
-
-  fileSystems = {
-    "/mnt/media" = {
-      device = "//pumpkin.lc.brotherwolf.ca/Media";
-      fsType = "cifs";
-      options = [
-        "credentials=${config.age.secrets.pumpkin-smb-credentials.path}"
-        "x-systemd.automount"
-        "noauto"
-        "nofail"
-        "uid=media"
-        "gid=media"
-        "file_mode=0660"
-        "dir_mode=0770"
-      ];
-    };
-    "/mnt/references" = {
-      device = "//pumpkin.lc.brotherwolf.ca/References";
-      fsType = "cifs";
-      options = [
-        "credentials=${config.age.secrets.pumpkin-smb-credentials.path}"
-        "x-systemd.automount"
-        "noauto"
-        "nofail"
-        "uid=media"
-        "gid=media"
-        "file_mode=0660"
-        "dir_mode=0770"
-      ];
-    };
-    "/mnt/immich" = {
-      device = "//pumpkin.lc.brotherwolf.ca/Immich";
-      fsType = "cifs";
-      options = [
-        "credentials=${config.age.secrets.pumpkin-smb-credentials.path}"
-        "x-systemd.automount"
-        "noauto"
-        "nofail"
-        "uid=immich"
-        "gid=media"
-        "file_mode=0660"
-        "dir_mode=0770"
-      ];
-    };
   };
 }
