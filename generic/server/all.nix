b32479d537af2c35652afd3dd004672d7e38b667
Daniel Innes <daniel@brotherwolf.ca>
2026-05-15 20:13:46 -0400

    calibre

--- generic/server/all.nix
+++ generic/server/all.nix
@@ -8,9 +8,7 @@ _:
     ./smb.nix
     ./jellyfin.nix
     ./dawarich.nix
-    # > Checking runtime dependencies for calibreweb-0.6.25-py3-none-any.whl
-    # >   - requests<2.33.0,>=2.32.0 not satisfied by version 2.33.1
-    # ./calibre.nix
+    ./calibre.nix
     ./immich.nix
     ./traefik.nix
     ./meilisearch.nix
