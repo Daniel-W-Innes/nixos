c4f58355b045885b032b35bb2d5d9acdbab50625
Daniel Innes <daniel@brotherwolf.ca>
2026-06-08 17:17:10 -0400

    go and gcc

--- home/vscode.nix
+++ home/vscode.nix
@@ -1,23 +1,30 @@
 { pkgs, ... }:
 
 {
-  programs.vscode = {
-    enable = true;
-    profiles.default = {
-      userSettings = {
-        "chat.agent.enabled" = false;
-        "editor.inlineSuggest.enabled" = true;
-        "merge-conflict.autoNavigateNextConflict.enabled" = true;
-        "git.autofetch" = true;
-        "git.confirmSync" = false;
-        "git.suggestSmartCommit" = false;
-        "harper.dialect" = "Canadian";
+  programs = {
+    vscode = {
+      enable = true;
+      profiles.default = {
+        userSettings = {
+          "chat.agent.enabled" = false;
+          "editor.inlineSuggest.enabled" = true;
+          "merge-conflict.autoNavigateNextConflict.enabled" = true;
+          "git.autofetch" = true;
+          "git.confirmSync" = false;
+          "git.suggestSmartCommit" = false;
+          "harper.dialect" = "Canadian";
+        };
+        extensions = with pkgs.vscode-extensions; [
+          # elijah-potter.harper removed for hash mismatch
+          golang.go
+          bbenoist.nix
+        ];
       };
-      extensions = with pkgs.vscode-extensions; [
-        # elijah-potter.harper removed for hash mismatch
-        golang.go
-        bbenoist.nix
-      ];
     };
+    go = {
+      enable = true;
+      telemetry.mode = "on";
+    };
+    gcc.enable = true;
   };
 }
