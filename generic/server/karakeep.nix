48a5289f63ffb3f14539214379a804cbd88e07df
Daniel Innes <daniel@brotherwolf.ca>
2026-06-21 19:41:57 -0400

    environment

--- generic/server/karakeep.nix
+++ generic/server/karakeep.nix
@@ -1,11 +1,9 @@
 { config, lib, ...}:
 
 {
-  services.karakeep = {
-    enable = true;
-    extraEnvironment = {
-      MEILI_ADDR = lib.mkForce "https://meilisearch.lc.brotherwolf.ca";
-      MEILI_MASTER_KEY_FILE = lib.mkForce config.age.secrets.meilisearch-masterKey.path;
-    };
+  services.karakeep.enable = true;
+  systemd.services.karakeep-web.environment = {
+    MEILI_ADDR = lib.mkForce "https://meilisearch.lc.brotherwolf.ca";
+    MEILI_MASTER_KEY_FILE = lib.mkForce config.age.secrets.meilisearch-masterKey.path;
   };
 }
