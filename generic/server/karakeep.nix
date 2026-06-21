08918496e0d457c406658fb05349d65300438273
Daniel Innes <daniel@brotherwolf.ca>
2026-06-21 19:39:30 -0400

    mkForce main meilisearch

--- generic/server/karakeep.nix
+++ generic/server/karakeep.nix
@@ -1,11 +1,11 @@
-{ config, ...}:
+{ config, lib, ...}:
 
 {
   services.karakeep = {
     enable = true;
     extraEnvironment = {
-      MEILI_ADDR = "https://meilisearch.lc.brotherwolf.ca";
-      MEILI_MASTER_KEY_FILE = config.age.secrets.meilisearch-masterKey.path;
+      MEILI_ADDR = lib.mkForce "https://meilisearch.lc.brotherwolf.ca";
+      MEILI_MASTER_KEY_FILE = lib.mkForce config.age.secrets.meilisearch-masterKey.path;
     };
   };
 }
