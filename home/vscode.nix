6c63bb1058f9ad3f460f7cebc18d18fb6e032ab4
Daniel Innes <daniel@brotherwolf.ca>
2026-07-01 12:08:57 -0400

    update and clean vscode config

--- home/vscode.nix
+++ home/vscode.nix
@@ -1,26 +1,8 @@
-{ pkgs, ... }:
+_:
 
 {
   programs = {
-    vscode = {
-      enable = true;
-      profiles.default = {
-        userSettings = {
-          "chat.agent.enabled" = false;
-          "editor.inlineSuggest.enabled" = true;
-          "merge-conflict.autoNavigateNextConflict.enabled" = true;
-          "git.autofetch" = true;
-          "git.confirmSync" = false;
-          "git.suggestSmartCommit" = false;
-          "harper.dialect" = "Canadian";
-        };
-        extensions = with pkgs.vscode-extensions; [
-          # elijah-potter.harper removed for hash mismatch
-          golang.go
-          bbenoist.nix
-        ];
-      };
-    };
+    vscode.enable = true;
     go = {
       enable = true;
       telemetry.mode = "on";
