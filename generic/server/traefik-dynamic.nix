fbad3b506056b7eea671f9bcfceea125ad76dd1d
Daniel Innes <daniel@brotherwolf.ca>
2026-08-03 21:34:56 -0400

    cipherSuites

--- generic/server/traefik-dynamic.nix
+++ generic/server/traefik-dynamic.nix
@@ -137,5 +137,24 @@ in
 
   tls.options.modern = {
     minVersion = "VersionTLS12";
+    cipherSuites = [
+      "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256"
+      "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"
+      "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384"
+      "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"
+      "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256"
+      "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256"
+    ];
+    curvePreferences = [
+      "X25519"
+      "CurveP256"
+      "CurveP384"
+      "CurveP521"
+    ];
+    sniStrict = true;
+    alpnProtocols = [
+      "h2"
+      "http/1.1"
+    ];
   };
 }
