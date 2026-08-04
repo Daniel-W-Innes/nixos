8bd5f31e989c95ce9390245c78122299288bab3a
Daniel Innes <daniel@brotherwolf.ca>
2026-08-03 20:35:16 -0400

    loosen the tls rules

--- generic/server/traefik.nix
+++ generic/server/traefik.nix
@@ -36,17 +36,25 @@
         };
       };
       tls.options.modern = {
-        minVersion = "VersionTLS13";
+        minVersion = "VersionTLS12";
         cipherSuites = [
+          "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256"
+          "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"
+          "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384"
+          "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"
+          "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305"
+          "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305"
           "TLS_AES_128_GCM_SHA256"
           "TLS_AES_256_GCM_SHA384"
           "TLS_CHACHA20_POLY1305_SHA256"
         ];
         curvePreferences = [
-          "CurveP521"
+          "X25519"
+          "CurveP256"
           "CurveP384"
+          "CurveP521"
         ];
-        sniStrict = true;
+        # sniStrict = true;   # removed – internal clients work with default cert
         forceSTSHeader = true;
         stsSeconds = 63072000;
         stsIncludeSubdomains = true;
