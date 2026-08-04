86f3d19d3a00b6bccf5fa920f4c7184d6c39861d
Daniel Innes <daniel@brotherwolf.ca>
2026-08-03 20:47:09 -0400

    remove cipherSuites and curvePreferences

--- generic/server/traefik.nix
+++ generic/server/traefik.nix
@@ -37,20 +37,7 @@
       };
       tls.options.modern = {
         minVersion = "VersionTLS12";
-        cipherSuites = [
-          "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256"
-          "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"
-          "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384"
-          "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"
-          "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305"
-          "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305"
-        ];
-        curvePreferences = [
-          "X25519"
-          "CurveP256"
-          "CurveP384"
-          "CurveP521"
-        ];
+        sniStrict = true;
         forceSTSHeader = true;
         stsSeconds = 63072000;
         stsIncludeSubdomains = true;
