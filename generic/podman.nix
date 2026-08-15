e36052b6a57ccbd6c7f05c476eef98d5e6019681
Daniel-W-Innes <daniel@brotherwolf.ca>
2026-08-15 11:52:07 -0400

    Disable aardvark-dns

--- generic/podman.nix
+++ generic/podman.nix
@@ -7,7 +7,7 @@
       enable = true;
       autoPrune.enable = true;
       dockerCompat = true;
-      defaultNetwork.settings.dns_enabled = true;
+      defaultNetwork.settings.dns_enabled = false;
     };
     oci-containers.backend = "podman";
   };
